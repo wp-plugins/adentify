f3b06c7eb951d69422edb33cce35acca2cabacc0
AdEntify <AdEntify@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-12 15:12:32 +0000

    tagging version 1.0.8
    git-svn-id: https://plugins.svn.wordpress.org/adentify/tags/1.0.8@1111179 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ public/Twig.php
@@ -0,0 +1,18 @@
+<?php
+
+class Twig
+{
+    public static function render($template_path, $parameters, $function = null)
+    {
+        $loader = new Twig_Loader_Filesystem(ADENTIFY__PLUGIN_DIR . 'templates');
+        $twig = new Twig_Environment($loader, array(
+            'cache' => WP_DEBUG ? false : ADENTIFY__PLUGIN_DIR . 'cache/templates',
+        ));
+        $twig->addExtension(new Twig_Extensions_Extension_I18n());
+        if ($function) {
+            $twig->addFunction(new Twig_SimpleFunction('wp_nonce_image_upload', $function));
+        }
+        $template = $twig->loadTemplate($template_path);
+        return $template->render($parameters);
+    }
+}
